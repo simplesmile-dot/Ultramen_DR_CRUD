3976c5c1c50428828f65ca6c42adfaaaca5d9bf0
Ahmad Zidane Sanduan <zidaneagil5@gmail.com>
2026-04-10 13:17:37 +0700

    commit 2

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-//commit 1
+//commit
 namespace CRUDMahasiswaADO
 {
     public partial class Form1 : Form
